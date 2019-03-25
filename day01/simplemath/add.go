9980f7e3ed736a03c44294a01518f88038a5793a
secondtonone1 <secondtonone1@163.com>
2019-03-25 15:02:35 +0800

    deleted:    day01/calcsrc/calc.go 	deleted:    day01/simplemath/add.go 	deleted:    day01/simplemath/sqrt.go 	deleted:    day02/variables.go 	deleted:    day03/slicemap.go 	deleted:    pkg/windows_amd64/mymaths.a

--- day01/simplemath/add.go
+++
@@ -1,4 +0,0 @@
-package simplemath
-func Add(a int, b int) int {
-	return a + b
-}
--- day01/simplemath/sqrt.go
+++
@@ -1,6 +0,0 @@
-package simplemath
-import "math"
-func Sqrt(i int) int {
-	v := math.Sqrt(float64(i))
-	return int(v)
-}
