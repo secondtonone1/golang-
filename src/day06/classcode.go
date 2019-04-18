f293343a0c524d22836285de088797d0b4f7b7c7
secondtonone1 <secondtonone1@163.com>
2019-04-18 21:52:28 +0800

    deleted:    src/day02/debug 	modified:   src/day06/classcode.go

--- src/day06/classcode.go
+++ src/day06/classcode.go
@@ -1,5 +1,6 @@
 package main
 import "fmt"
+//import "log"
 type Integer int 
 func (a Integer) Less (b Integer) bool{
     return a < b
@@ -37,6 +38,27 @@ func (foo* Foo) Foo(){
     foo.Base.Foo()
     fmt.Println("this is Foo Foo")
 }
+
+//匿名指针组合
+type DerivePoint struct{
+    * Base 
+}
+
+func (derivep * DerivePoint) Foo() bool{
+    fmt.Println("this is DerivePoint Foo")
+    return true
+}
+
+type Logger struct {
+    Level int
+}
+//重复定义，因为匿名组合默认用类型做变量名
+type MyJob struct {
+    *Logger
+    Name string
+    //*log.Logger // duplicate field Logger
+}
+
 
 //同名成员覆盖，基类成员被隐藏
 type X struct{
@@ -84,4 +106,20 @@ y.X.Name = "x.Name"
 name1 := y.GetXName()
 name2 := y.GetName()
 fmt.Println("name1 is ",name1, "name2 is ",name2)
+
+derivep := new(DerivePoint)
+derivep.Base = new(Base)
+derivep.Base.Name="derivep.Base.Name"
+fmt.Println("derivep.Base.Name is ", derivep.Base.Name, " derivep.Name is ", derivep.Name)
+derivep.Name="derivep.Name"
+fmt.Println("derivep.Base.Name is ", derivep.Base.Name, " derivep.Name is ", derivep.Name)
+derivep.Foo()
+derivep.Base.Foo()
+
+derivep2 := DerivePoint{&Base{Name:"derivepbase2"}}
+derivep2.Foo()
+fmt.Println("derivep2 base name is ", derivep2.Base.Name)
+fmt.Println("derivep2 name is ", derivep2.Name)
+
 }
+
