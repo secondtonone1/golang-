e205ca8c5a5244d39be0eaafb020ef1383fa986e
secondtonone1 <secondtonone1@163.com>
2019-04-26 18:53:22 +0800

    new file:   src/day08/consignment/consignment.proto 	new file:   src/day08/consignment/pbgenerator.bat 	new file:   src/day08/grpcclient/client.go 	new file:   src/day08/grpcclient/consignment.json 	new file:   src/day08/grpcserver/server.go

---
+++ src/day08/grpcclient/client.go
@@ -0,0 +1,58 @@
+// consignment-service/main.go
+// consignment-cli/cli.go
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "day08/consignment"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+	// 创建和服务器的一个连接
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewShippingServiceClient(conn)
+
+	// 和服务器通信，并打印出返回信息
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+}
---
+++ src/day08/grpcserver/server.go
@@ -0,0 +1,73 @@
+// consignment-service/main.go
+package main
+
+import (
+	"log"
+	"net"
+
+	// 导入生成的 protobuf 代码
+	pb "day08/consignment"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type IRepository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+// Repository - 一个模拟数据存储的虚拟仓库，以后我们会替换成真实的数据仓库
+type Repository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+// 服务需要实现所有在 protobuf 里定义的方法。
+// 你可以参考 protobuf 生成的 go 文件中的接口信息。
+type service struct {
+	repo IRepository
+}
+
+// CreateConsignment - 目前只创建了这个方法，包括 `ctx` (环境信息)和 `req`(委托请求)两个参数，会通过 gRPC 服务器进行处理
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+
+	// 保存委托
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// 返回和 protobuf 中定义匹配的 `Response` 消息
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+
+	repo := &Repository{}
+
+	// 启动 gRPC 服务器。
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	// 注册服务到 gRPC 服务器，会把已定义的 protobuf 与自动生成的代码接口进行绑定。
+	pb.RegisterShippingServiceServer(s, &service{repo})
+
+	// 在 gRPC 服务器上注册 reflection 服务。
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
