7bb9b7cd288908aa358d7cf2347d10c74ff44bb0
secondtonone1 <secondtonone1@163.com>
2019-06-27 14:57:27 +0800

    new file:   src/day21/heapsort/heapsort.go

---
+++ src/day21/heapsort/heapsort.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+type HeapSort struct {
+}
+
+//调整index为根的子树
+func (hs *HeapSort) adjustHeap(array []int, index, length int) {
+	//index 的左右子节点
+	leftchild := index*2 + 1
+	rightchild := leftchild + 1
+	maxchild := leftchild
+
+	for {
+
+		//如果左节点比长度大，说明该节点为子节点
+		if leftchild > length-1 {
+			break
+		}
+		//右节点存在，且比左节点大
+		if rightchild <= length-1 && array[rightchild] > array[maxchild] {
+			maxchild = rightchild
+		}
+
+		//比较自己元素和最大节点的元素，做交换
+		if array[index] < array[maxchild] {
+			hs.swap(array, index, maxchild)
+		}
+		index = maxchild
+		leftchild = index*2 + 1
+		rightchild = leftchild + 1
+		maxchild = leftchild
+	}
+
+}
+
+func (hs *HeapSort) swap(array []int, i, j int) {
+	if i >= len(array) || j >= len(array) {
+		return
+	}
+
+	temp := array[i]
+	array[i] = array[j]
+	array[j] = temp
+
+}
+
+func (hs *HeapSort) sort(array []int) {
+
+	//每次循环后长度减少，因为每次循环最后元素都变为最大
+	for length := len(array); length > 1; length-- {
+		//最后一个非叶子节点索引
+		lastnode := length/2 - 1
+		//从最后一个非叶子节点一次从左到右，从下到上排序子树
+		//循环过后得到一个大顶堆(此时还不是大根堆)
+		for i := lastnode; i >= 0; i-- {
+			hs.adjustHeap(array, i, length)
+		}
+
+		//将堆顶元素放到末尾
+		hs.swap(array, 0, length-1)
+		fmt.Println(array)
+	}
+
+}
+
+func main() {
+
+	//数组初始化
+	array := []int{6, 1, 0, 5, 2, 9, 6}
+	hs := new(HeapSort)
+	hs.sort(array)
+
+}
